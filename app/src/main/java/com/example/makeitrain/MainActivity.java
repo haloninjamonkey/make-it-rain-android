7fedb2dd0a0c21c0b237b79a54b63e1d04a5487e
Patrick Duff <patrick.duff@bodybuilding.com>
2019-09-06 16:47:46 -0600

    initial commit

---
+++ app/src/main/java/com/example/makeitrain/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.makeitrain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button showMoney;
+    private Button showTag;
+    private TextView moneyText;
+    private TextView winningText;
+    private int moneyCounter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showMoney = findViewById(R.id.button_make_rain);
+        showTag = findViewById(R.id.button_show_tag);
+        moneyText = findViewById(R.id.money_text);
+        winningText = findViewById(R.id.winning_text);
+
+
+
+//        showMoney.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+//
+//        showTag.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+    }
+
+    public void showTag(View v){
+        Toast.makeText(getApplicationContext(), R.string.app_name , Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    public void makeItRain(View v){
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+        moneyCounter += 1000;
+        if(moneyCounter < 21000) {
+            moneyText.setText(String.valueOf(numberFormat.format(moneyCounter)));
+        } else {
+            moneyText.setTextColor(Color.WHITE);
+            winningText.setText(R.string.winner);
+            moneyText.setText(String.valueOf(numberFormat.format(moneyCounter)));
+        }
+        Log.d("MYTAG", "onClick: Show Money" + moneyCounter);
+    }
+}
